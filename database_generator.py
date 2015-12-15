c1f83fb258a6704f4464ed720907cf72d7b686cb
Tom LaTulipe <tlatulipe@gmail.com>
2015-12-15 15:51:15 -0500

    changed conn.execute to use different formatting (not using {} with .format)

--- database_generator.py
+++ database_generator.py
@@ -1,6 +1,8 @@
 ﻿import sqlite3
 import os	
 import eyed3
+import codecs
+import sys
 
 
 conn = sqlite3.connect('E:\Music\music.db')
@@ -30,17 +32,29 @@ for file in os.listdir(os.getcwd()):
 '''
 
 count = 0
+problem = codecs.open('E:\Music\problem_children.txt', 'w', encoding ='utf-8')
 
 for dirName, subdirList, fileList in os.walk(music_dir):
     for fname in fileList:
         if fname.endswith(".mp3"):
             audiofile = eyed3.load(os.path.join(dirName,fname))
             try:
+                '''
                 conn.execute('INSERT INTO MUSIC (ID, ARTIST, ALBUM, TITLE, TRACK) \
-		        VALUES ({},"{}","{}","{}",{})'.format(count,audiofile.tag.artist,audiofile.tag.album, audiofile.tag.title,audiofile.tag.track_num[0]));
+		        VALUES ({},"{}","{}","{}",{})'.format(count,unicode(audiofile.tag.artist),unicode(audiofile.tag.album), unicode(audiofile.tag.title),audiofile.tag.track_num[0]));
                 count += 1
+                '''
+                conn.execute('INSERT INTO MUSIC (ID, ARTIST, ALBUM, TITLE, TRACK) \
+                VALUES (?, ?, ?, ?, ?)', (count,audiofile.tag.artist,audiofile.tag.album, audiofile.tag.title,audiofile.tag.track_num[0]))
+                count += 1
+
             except:
-                pass    
+                problem.write(os.path.join(dirName, fname))
+                problem.write(u'\r\n')
+                problem.write(unicode(sys.exc_info()[0]))
+                problem.write(u'\r\n')
+                problem.flush()   
 
 conn.commit()
 conn.close()
+problem.close()
